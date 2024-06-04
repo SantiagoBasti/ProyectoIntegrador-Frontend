ce72f74db1e6ff698df973c662fd7c27ec65ea34
Santiago <santiago.bastidascaro@gmail.com>
2024-06-03 22:07:40 -0500

    header basico

--- src/layout/header/Header.jsx
+++ src/layout/header/Header.jsx
@@ -1,45 +1,39 @@
-import { useState } from "react";
 import { NavLink } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars, faTimes, faCartShopping } from "@fortawesome/free-solid-svg-icons";
 import "./Header.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 
-export default function Header() {
+export default function Header(){
     const isAdmin = true;
-    const [showMenu, setShowMenu] = useState(false);
 
-    return (
-        <header className="header">
-            <div className="logo">
-                <img src="https://i.pinimg.com/originals/4f/f5/09/4ff509a7f661b99574cb6b0d86e14232.png" alt="" />
-            </div>
-            <nav className={`header-nav ${showMenu ? "show-menu" : ""}`}>
-                <div className="nav-links">
-                    <NavLink to="/" className="nav-link" onClick={() => setShowMenu(false)}>
-                        Home
-                    </NavLink>
-                    <NavLink to="/register" className="nav-link" onClick={() => setShowMenu(false)}>
-                        Registro
-                    </NavLink>
-                    {isAdmin && (
-                        <>
-                            <NavLink to="/admin-product" className="nav-link" onClick={() => setShowMenu(false)}>
-                                Admin Product
-                            </NavLink>
-                            <NavLink to="/admin-user" className="nav-link" onClick={() => setShowMenu(false)}>
-                                Admin Users
-                            </NavLink>
-                        </>
-                    )}
-                </div>
-            </nav>
-            <div className={`menu-icon ${showMenu ? "open" : ""}`} onClick={() => setShowMenu(!showMenu)}>
-                <FontAwesomeIcon icon={showMenu ? faTimes : faBars} />
-            </div>
-            
-            <div className="user-info">
-                <FontAwesomeIcon icon={faCartShopping} />
-            </div>
-        </header>
-    );
+
+    return(
+    <>
+    <header>
+        <img src="https://i.pinimg.com/originals/4f/f5/09/4ff509a7f661b99574cb6b0d86e14232.png" alt="" />
+        <nav className="header-nav">
+        <NavLink to="/" className="nav-link">
+            Home
+          </NavLink>
+          <NavLink to="/register" className="nav-link">
+            Registro
+          </NavLink>
+          {isAdmin && (
+            <>
+              <NavLink to="/admin-product" className="nav-link">
+                Admin Product
+              </NavLink>
+              <NavLink to="/admin-user" className="nav-link">
+                Admin Users
+              </NavLink>
+            </>
+          )}
+        </nav>
+
+        <div className="user-info">
+          <FontAwesomeIcon icon={faCartShopping}/>
+        </div>
+    </header>
+    </>
+    )
 }
