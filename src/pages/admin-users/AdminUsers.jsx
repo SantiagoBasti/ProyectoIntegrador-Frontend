3f9cd023a92c53c5af9b65ec153d7fde897a452a
Santiago <santiago.bastidascaro@gmail.com>
2024-06-13 16:10:11 -0500

    se arreglo la tabla de admin user falta poner responsive la tabla.

--- src/pages/admin-users/AdminUsers.jsx
+++ src/pages/admin-users/AdminUsers.jsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import Swal from 'sweetalert2';
+import { useForm } from 'react-hook-form';
 import './AdminUsers.css';
 import Icon from 'react-icons-kit';
 import { bin, pencil } from "react-icons-kit/icomoon";
-import {formatDateUser} from '../../services/utils/DateUser';
+import { formatDateUser } from '../../services/utils/DateUser';
 
 export default function AdminUsers() {
   const URL = `https://665e339a1e9017dc16ef5241.mockapi.io/`;
@@ -13,16 +14,25 @@ export default function AdminUsers() {
     fullname: "",
     bornDate: "",
     email: "",
-    phone: "",
     image: "",
+    location: "",
+    password: ""
   };
 
   const [users, setUsers] = useState([]);
   const [formContact, setFormContact] = useState(defaultFormValues);
 
+  const { register, handleSubmit, setValue, reset, formState: { errors } } = useForm({
+    defaultValues: formContact
+  });
+
   useEffect(() => {
     getContactList();
   }, []);
+
+  useEffect(() => {
+    reset(formContact);
+  }, [formContact, reset]);
 
   async function getContactList() {
     try {
@@ -37,16 +47,14 @@ export default function AdminUsers() {
     }
   }
 
-  function handleSubmit(evt) {
-    evt.preventDefault();
-    const el = evt.target.elements;
-
+  const handleFormSubmit = (data) => {
     const newContact = {
-      fullname: el.fullname.value,
-      bornDate: new Date(el.bornDate.value).getTime(),
-      email: el.email.value,
-      phone: el.phone.value,
-      image: el.image.value,
+      fullname: data.fullname,
+      bornDate: new Date(data.bornDate).getTime(),
+      email: data.email,
+      image: data.image,
+      location: data.location,
+      password: data.password,
     };
 
     if (formContact.id) {
@@ -54,7 +62,7 @@ export default function AdminUsers() {
     } else {
       createNewContact(newContact);
     }
-  }
+  };
 
   async function updateContact(contacto) {
     try {
@@ -74,11 +82,6 @@ export default function AdminUsers() {
     } catch (error) {
       Swal.fire("Error", "No se pudo crear el contacto", "icon");
     }
-  }
-
-  function handleChange(e) {
-    const { name, value } = e.target;
-    setFormContact((previousValue) => ({ ...previousValue, [name]: value }));
   }
 
   function handleDelete(id) {
@@ -101,41 +104,101 @@ export default function AdminUsers() {
   }
 
   function handleEdit(contact) {
-    setFormContact({ ...contact });
+    setFormContact({
+      ...contact,
+      bornDate: new Date(contact.bornDate).toISOString().split('T')[0]
+    });
+    for (const key in contact) {
+      setValue(key, contact[key]);
+    }
   }
 
   return (
     <>
       <div className="contact-list-container">
-        {/* ===========================Formulario anadir contactos====================== */}
+        {/* ===========================Formulario añadir contactos====================== */}
         <div className="contact-form-wrapper">
-          <h1>Añadir Contactos</h1>
-          <form className="contact-form" onSubmit={handleSubmit}>
+          <h1>{formContact.id ? "Editar contacto" : "Añadir contacto"}</h1>
+          <form className="contact-form" onSubmit={handleSubmit(handleFormSubmit)}>
             <input type="hidden" name="id" />
 
             <div className="input-group">
               <label htmlFor="fullname">Nombre Completo</label>
-              <input value={formContact.fullname} onChange={handleChange} type="text" id="fullname" name="fullname" />
+              <input
+                placeholder="Ingrese el nombre completo"
+                {...register('fullname', {
+                  required: 'El nombre completo es requerido',
+                  minLength: {
+                    value: 4,
+                    message: 'La cantidad de caracteres es inválida'
+                  },
+                  maxLength: {
+                    value: 35,
+                    message: 'El nombre es demasiado largo'
+                  }
+                })}
+              />
+              {errors.fullname && <span className='error-message'>{errors.fullname.message}</span>}
             </div>
 
             <div className="input-group">
-              <label htmlFor="bornDate">Fecha de nacimiento</label>
-              <input type="date" id="bornDate" name="bornDate" value={formatDateUser(formContact.bornDate)} onChange={handleChange} />
+              <label htmlFor="email">Correo Electrónico</label>
+              <input
+                placeholder="Ingrese el correo electrónico"
+                {...register('email', { required: 'El correo electrónico es requerido' })}
+              />
+              {errors.email && <span className='error-message'>{errors.email.message}</span>}
             </div>
 
             <div className="input-group">
-              <label htmlFor="email">Correo Electronico</label>
-              <input value={formContact.email} onChange={handleChange} type="email" id="email" name="email" />
+              <label htmlFor="password">Contraseña</label>
+              <input
+                type="password"
+                placeholder="Ingrese la contraseña"
+                {...register('password', { required: 'La contraseña es requerida' })}
+              />
+              {errors.password && <span className='error-message'>{errors.password.message}</span>}
             </div>
 
             <div className="input-group">
-              <label htmlFor="phone">Telefono</label>
-              <input value={formContact.phone} onChange={handleChange} type="text" id="phone" name="phone" />
+              <label htmlFor="location">Localización</label>
+              <select
+                placeholder="Seleccione un país"
+                {...register('location', { required: 'La localización es requerida' })}
+              >
+                <option value="">Seleccione un país</option>
+                <option value="Argentina">Argentina</option>
+                <option value="Bolivia">Bolivia</option>
+                <option value="Brasil">Brasil</option>
+                <option value="Chile">Chile</option>
+                <option value="Colombia">Colombia</option>
+                <option value="Ecuador">Ecuador</option>
+                <option value="Paraguay">Paraguay</option>
+                <option value="Peru">Perú</option>
+                <option value="Uruguay">Uruguay</option>
+                <option value="Venezuela">Venezuela</option>
+              </select>
+              {errors.location && <span className='error-message'>{errors.location.message}</span>}
+            </div>
+
+            <div className="input-group">
+              <label htmlFor="bornDate">Fecha de Nacimiento</label>
+              <input
+                type="date"
+                placeholder="Ingrese la fecha de nacimiento"
+                {...register('bornDate', { required: 'La fecha de nacimiento es requerida' })}
+              />
+              {errors.bornDate && <span className='error-message'>{errors.bornDate.message}</span>}
             </div>
 
             <div className="input-group">
               <label htmlFor="image">Enlace a la Imagen</label>
-              <input value={formContact.image} onChange={handleChange} type="url" id="image" name="image" />
+              <input
+                type="url"
+                placeholder="Ingrese el enlace a la imagen"
+                {...register('image', { required: 'El enlace a la imagen es requerido' })}
+              />
+              {errors.image && <span className='error-message'>{errors.image.message}</span>}
             </div>
 
             <div className="input-group">
@@ -147,31 +210,43 @@ export default function AdminUsers() {
         </div>
 
         {/* ========================= Lista de contactos========================= */}
-        <div className="contact-list">
-          <h1>Lista de Contactos</h1>
-          <ul>
-            {users.map((usuario) => (
-              <li key={usuario.id} className="contact">
-                <div className="contact-image-wrapper">
-                  <img className="contact-image" src={usuario.image} alt={usuario.fullname} />
-                </div>
-                <div className="contact-user">
-                  <div className="contact-fullname">{usuario.fullname}</div>
-                  <span className="contact-date">{usuario.bornDate}</span>
-                </div>
-                <div className="contact-email">{usuario.email}</div>
-                <div className="contact-phone">{usuario.phone}</div>
-                <div className="contact-actions">
-                  <button onClick={() => handleEdit(usuario)}>
-                    <Icon icon={pencil} />
-                  </button>
-                  <button className="danger" onClick={() => handleDelete(usuario.id)}>
-                    <Icon icon={bin} />
-                  </button>
-                </div>
-              </li>
-            ))}
-          </ul>
+        <div className="table-wrapper">
+          <div className="contact-table">
+            <h1>Lista de Contactos</h1>
+            <table>
+              <thead>
+                <tr>
+                  <th>Imagen</th>
+                  <th>Nombre Completo</th>
+                  <th>Email</th>
+                  <th>Localización</th>
+                  <th>Fecha de Nacimiento</th>
+                  <th>Acciones</th>
+                </tr>
+              </thead>
+              <tbody>
+                {users.map((usuario) => (
+                  <tr key={usuario.id} className="contact">
+                    <td className="contact-image-wrapper">
+                      <img className="contact-image" src={usuario.image} alt={usuario.fullname} />
+                    </td>
+                    <td className="contact-fullname">{usuario.fullname}</td>
+                    <td className="contact-email">{usuario.email}</td>
+                    <td className="contact-location">{usuario.location}</td>
+                    <td className="contact-date">{formatDateUser(usuario.bornDate)}</td>
+                    <td className="contact-actions">
+                      <button onClick={() => handleEdit(usuario)}>
+                        <Icon icon={pencil} />
+                      </button>
+                      <button className="danger" onClick={() => handleDelete(usuario.id)}>
+                        <Icon icon={bin} />
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     </>
--- src/services/utils/DateUser.jsx
+++ src/services/utils/DateUser.jsx
@@ -3,7 +3,7 @@ export function formatDateUser(date){
         const dateObj = date ? new Date(date) : new Date();
     
         const year = dateObj.getUTCFullYear();
-        let month = dateObj.getUTCMonth();
+        let month = dateObj.getUTCMonth() + 1;
         let day = dateObj.getUTCDate();
     
         if(month < 10){
@@ -14,17 +14,4 @@ export function formatDateUser(date){
         }
     
         return `${year}-${month}-${day}`
-    
-    }
-    
-    export function formatDate(date){
-        if(!date) return "-";
-    
-        const collator = new Intl.DateTimeFormat("es-AR",{
-            day: "2-digit",
-            month: "2-digit",
-            year: "numeric"
-        })
-    
-        return collator.format(date)
     }
